825cec77e9c1e77d522b271bf48224829a8a670f
Leandro Schillreff <leandroschillreff@gmail.com>
2024-09-26 15:32:52 -0400

    feat: global and reset style

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-primary: #6328A4;
+    --color-primary-focus: #5E4D80;
+    --color-primary-negative: #5E4D80;
+    --color-gray-0: #F8F9FA;
+    --color-gray-1: #868E96;
+    --color-gray-2: #343B41;
+    --color-gray-3: #212529;
+    --color-gray-4: #121214;
+    --color-sucess: #3FE864;
+    --color-negative: #E83F5B;
+}`;
