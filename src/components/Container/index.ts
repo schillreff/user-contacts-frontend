8b373a0ed4ef2e6d573f633d91e9f98ab6683afb
Leandro Schillreff <leandroschillreff@gmail.com>
2024-09-27 13:53:15 -0400

    feat: container

---
+++ src/components/Container/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 1rem;
+`;
